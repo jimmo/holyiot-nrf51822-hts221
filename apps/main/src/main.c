93bceeb5e1a6fc16456ac27110a488c83f2fa298
Jim Mussared <jim.mussared@gmail.com>
2020-06-11 09:56:45 +1000

    Increase tx power. Add unique name.

--- apps/main/src/main.c
+++ apps/main/src/main.c
@@ -23,16 +23,19 @@
 #include "sysinit/sysinit.h"
 #include "os/os.h"
 #include "bsp/bsp.h"
+#include "hal/hal_bsp.h"
 #include "hal/hal_gpio.h"
 #include "hal/hal_i2c.h"
 #include "console/console.h"
 #include "host/ble_hs.h"
 
-
 #define HTS_DRDY_PIN (28)
 #define HTS_CS_PIN (24)
-static bool hts_init_complete = 0;
 
+// Flag to enable DRDY irq.
+static volatile bool hts_init_complete = 0;
+
+// Values extracted from HTS221 registers.
 static uint8_t H0_rH_x2 = 0;
 static uint8_t H1_rH_x2 = 0;
 
@@ -45,9 +48,13 @@ static int16_t H1_T0_OUT = 0;
 static int16_t T0_OUT = 0;
 static int16_t T1_OUT = 0;
 
-static int16_t H_rH_x2;
-static int16_t T_degC_x8;
+// Computed values after applying calibration, ready for sending over BLE.
+static volatile int16_t H_rH_x2 = 0;
+static volatile int16_t T_degC_x8 = 0;
 
+static void ble_app_advertise(int32_t duration_ms);
+
+// Generate non-resolvable private random address.
 static void ble_app_set_addr(void) {
     ble_addr_t addr;
     int rc;
@@ -59,8 +66,7 @@ static void ble_app_set_addr(void) {
     assert(rc == 0);
 }
 
-static void ble_app_advertise(int32_t duration_ms);
-
+// At the end of each advertising period, start a new one.
 static int gap_event_cb(struct ble_gap_event *event, void *arg) {
     if (event->type == BLE_GAP_EVENT_ADV_COMPLETE) {
         ble_app_advertise(5000);
@@ -68,38 +74,67 @@ static int gap_event_cb(struct ble_gap_event *event, void *arg) {
     return 0;
 }
 
+// For encoding the device unique ID.
+static uint8_t to_hex_nibble(uint8_t v) {
+    if (v < 10) {
+        return '0' + v;
+    } else {
+        return 'a' + (v-10);
+    }
+}
+
+static void to_hex(uint8_t v, uint8_t *out) {
+    out[0] = to_hex_nibble((v) & 0xf);
+    out[1] = to_hex_nibble((v >> 4) & 0xf);
+}
+
 static uint8_t adv_data[] = {
+    // BLE-only
     0x02, 0x01, 0x06,
-    0x07, 0xff, 0xff, 0xff, 0x01, 0x02, 0x01, 0x02,
-    0x05, 0x09, 't', 'e', 'm', 'p',
+    // Manufacturing data with four-byte payload.
+    0x07, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00,
+    // Device local name with 8 hex digits of unique ID.
+    0x0b, 0x09, 't', 'h', '0', '0', '0', '0', '0', '0', '0', '0'
 };
 
-static void
-ble_app_advertise(int32_t duration_ms) {
+// Start advertising.
+static void ble_app_advertise(int32_t duration_ms) {
     int rc;
 
+    hal_gpio_write(LED_BLINK_PIN, 1);
+
+    // Stop any existing advertising (there shouldn't be any).
     if (ble_gap_adv_active()) {
         ble_gap_adv_stop();
     }
 
+    // Copy the current temp/humidity values into the payload.
     memcpy(adv_data + 7, &H_rH_x2, 2);
     memcpy(adv_data + 9, &T_degC_x8, 2);
 
+    // Set payload.
     rc = ble_gap_adv_set_data(adv_data, sizeof(adv_data));
-
     rc = ble_gap_adv_rsp_set_data(NULL, 0);
 
-    int32_t interval_us = 250000;
+    // Every 300-600ms (for 5 seconds).
+    int32_t interval_us = 500000;
 
     struct ble_gap_adv_params adv_params = {
+        // Non-connectable.
         .conn_mode = BLE_GAP_CONN_MODE_NON,
+        // General-discoverable.
         .disc_mode = BLE_GAP_DISC_MODE_GEN,
+        // Interval
         .itvl_min = interval_us / BLE_HCI_ADV_ITVL, // convert to 625us units.
-        .itvl_max = interval_us / BLE_HCI_ADV_ITVL,
-        .channel_map = 7, // all 3 channels.
+        .itvl_max = (interval_us * 2) / BLE_HCI_ADV_ITVL,
+        // All 3 adv channels.
+        .channel_map = 7,
     };
 
+    // Start advertising.
     rc = ble_gap_adv_start(BLE_OWN_ADDR_RANDOM, NULL, duration_ms, &adv_params, gap_event_cb, NULL);
+
+    hal_gpio_write(LED_BLINK_PIN, 0);
 
     assert(rc == 0);
 }
@@ -112,6 +147,7 @@ static void ble_app_on_sync(void) {
     ble_app_advertise(5000);
 }
 
+// Write to a single-byte register on the HTS221.
 static void hts_write_reg(uint8_t reg, uint8_t value) {
     int rc;
 
@@ -126,6 +162,7 @@ static void hts_write_reg(uint8_t reg, uint8_t value) {
     assert(rc == 0);
 }
 
+// Read from a single-byte register on the HTS221.
 static uint8_t hts_read_reg(uint8_t reg) {
     int rc;
 
@@ -141,6 +178,7 @@ static uint8_t hts_read_reg(uint8_t reg) {
     return reg;
 }
 
+// Read len bytes from a register on the HTS221.
 static void hts_read_bytes(uint8_t reg, uint8_t *buf, uint8_t len) {
     int rc;
 
@@ -158,6 +196,7 @@ static void hts_read_bytes(uint8_t reg, uint8_t *buf, uint8_t len) {
     assert(rc == 0);
 }
 
+// Read a single signed int16 from a register on the HTS221.
 static int16_t hts_read_reg_s16(uint8_t reg) {
     uint8_t buf[2];
     hts_read_bytes(reg, buf, 2);
@@ -165,49 +204,50 @@ static int16_t hts_read_reg_s16(uint8_t reg) {
     return *((int16_t*)&v);
 }
 
+// Edge triggered IRQ for DRDY pin on the HTS221.
 void drdy_irq(void *arg) {
     if (!hts_init_complete) {
         return;
     }
 
-    hal_gpio_write(LED_BLINK_PIN, 1);
-
+    // Read current value registers (will clear "pending" state, allowing another DRDY irq).
     int32_t H_OUT = hts_read_reg_s16(0x28);
     int32_t T_OUT = hts_read_reg_s16(0x2a);
 
+    // Apply calibration.
     H_rH_x2 = H0_rH_x2 + ((H_OUT - H0_T0_OUT) * (H1_rH_x2 - H0_rH_x2)) / (H1_T0_OUT - H0_T0_OUT);
     T_degC_x8 = T0_degC_x8 + ((T_OUT - T0_OUT) * (T1_degC_x8 - T0_degC_x8)) / (T1_OUT - T0_OUT);
-
-    hal_gpio_write(LED_BLINK_PIN, 0);
 }
 
 static int hts_init(void) {
     int rc;
 
+    // Enable DRDY irq.
     hal_gpio_irq_init(HTS_DRDY_PIN, &drdy_irq, NULL, HAL_GPIO_TRIG_RISING, HAL_GPIO_PULL_NONE);
     hal_gpio_irq_enable(HTS_DRDY_PIN);
 
     // Enable I2C mode.
     hal_gpio_init_out(HTS_CS_PIN, 1);
-
     os_time_delay(OS_TICKS_PER_SEC / 4);
 
+    // Verify that the device is available.
     rc = hal_i2c_master_probe(0, 0x5f, OS_TICKS_PER_SEC);
     assert(rc == 0);
 
+    // Configure device.
     hts_write_reg(0x10, 0x1b); // Averaging conf, temp=16 hum=32 0.03deg 0.15rH% 2.10uA
     hts_write_reg(0x20, 0x85); // Control reg 1, power-on, block-data-update, 1Hz
     hts_write_reg(0x22, 0x04); // Control reg 3, Data ready active-high, push-pull, enable
-
     os_time_delay(OS_TICKS_PER_SEC / 2);
 
+    // Verify it's the right device.
     uint8_t whoami = hts_read_reg(0x0f);
-
     assert(whoami == 0xbc);
     if (whoami == 0xbc) {
         hts_init_complete = true;
     }
 
+    // Read calibration data.
     H0_rH_x2 = hts_read_reg(0x30);
     H1_rH_x2 = hts_read_reg(0x31);
     T0_degC_x8 = hts_read_reg(0x32);
@@ -220,6 +260,7 @@ static int hts_init(void) {
     T0_OUT = hts_read_reg_s16(0x3c);
     T1_OUT = hts_read_reg_s16(0x3e);
 
+    // Read the current value (to clear "pending" state).
     hts_read_reg_s16(0x28);
     hts_read_reg_s16(0x2a);
 
@@ -229,14 +270,28 @@ static int hts_init(void) {
 int main(int argc, char **argv) {
     int rc;
 
+    // MyNewt init.
     sysinit();
 
+    // LED for startup (will be cleared on first advertising).
     hal_gpio_init_out(LED_BLINK_PIN, 1);
 
+    // Initialise temp/humidity sensor.
     hts_init();
 
+    // Get device unique ID from nRF.
+    uint8_t uid[4];
+    int uid_len = min(hal_bsp_hw_id_len(), sizeof(uid));
+    rc = hal_bsp_hw_id(uid, uid_len);
+    assert(rc == 4);
+    for (int i = 0; i < 4; ++i) {
+        to_hex(uid[i], adv_data + i * 2 + 15);
+    }
+
+    // Enable BLE.
     ble_hs_cfg.sync_cb = ble_app_on_sync;
 
+    // Run message loop.
     while (1) {
         os_eventq_run(os_eventq_dflt_get());
     }
--- client/scan.py
+++ client/scan.py
@@ -3,14 +3,25 @@ import struct
 
 from bleak import discover
 
+NAMES = {
+    'th64d28b01': 'outside',
+    'th4a011940': 'kitchen',
+    'thc37fd516': 'taylor bedroom',
+    'th4951ba35': 'taylor bathroom',
+}
+
 async def run():
     while True:
+        print()
         devices = await discover()
+        seen = set()
         for d in devices:
+            #print(d)
             manuf = d.metadata.get('manufacturer_data', {}).get(65535, [])
-            if d.name.startswith('temp') and len(manuf) == 4:
+            if d.name not in seen and d.name.startswith('th') and len(d.name) == 10 and len(manuf) == 4:
                 h, t = struct.unpack('<hh', bytes(manuf))
-                print(d.name, h / 2, t / 8)
+                print(d.rssi, NAMES.get(d.name, d.name), h / 2, t / 8)
+                seen.add(d.name)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run())
